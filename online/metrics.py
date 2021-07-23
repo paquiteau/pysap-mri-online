fb9521e89f8131d49fb0ab31c1f743cfd9f7cc09
Pierre-antoine Comby <comby@crans.org>
2021-07-23 20:53:37 +0200

    use custom metrics without  scaling

--- online/metrics.py
+++ online/metrics.py
@@ -1,19 +1,32 @@
 import numpy as np
-from modopt.math.metrics import psnr, ssim
+from modopt.math.metrics import psnr, ssim, mse
 import scipy.fftpack as pfft
 
 
 def ssos(img):
     if len(img.shape) == 3:
-        return np.sqrt(np.sum(img ** 2, axis=0))
+        return np.sqrt(np.sum(abs(img) ** 2, axis=0))
     else:
-        return np.sqrt(img ** 2)
+        return abs(img)
+
 
 def psnr_ssos(test, ref, mask=None):
     test = ssos(test)
-    return psnr(test, ref,mask=None)
+    if mask is not None:
+        mse = np.mean(np.square(mask*(test-ref)))
+        return 10*np.log10(np.max(ref*mask)**2/mse)
+    else:
+        return 10*np.log10(np.max(ref)**2/np.mean(np.square(test-ref)))
 
 
 def ssim_ssos(test, ref, mask=None):
     test = ssos(test)
-    return ssim(test, ref, mask=None)
+    return ssim(test, ref, mask=mask)
+
+
+def mse_ssos(test, ref, mask=None):
+    test = ssos(test)
+    if mask is not None:
+        return np.mean(np.square(mask*(test-ref)))
+    else:
+        return np.mean(np.square(test-ref))
